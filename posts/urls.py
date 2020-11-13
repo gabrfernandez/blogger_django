eebb88d7c3aa35ef8a25ec503e0056994f31f277
gabrfernandez <61893056+gabrfernandez@users.noreply.github.com>
2020-11-13 15:16:58 -0800

    views and templates

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from posts.views import *
 
 urlpatterns=[
-    path('', IndexView.as_view(), name="index")
+    path('', IndexView.as_view(), name="index"),
+    path('detail/<int:pk>', PostDetail.as_view(), name="detail")
 ]

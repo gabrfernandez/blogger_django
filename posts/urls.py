7f11a71e66bc00128a319e9bfaca81b4b2542e65
gabrfernandez <61893056+gabrfernandez@users.noreply.github.com>
2020-11-12 22:09:38 -0800

    templates

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from posts.views import *
 
 urlpatterns=[
-
+    path('', IndexView.as_view(), name="index")
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+# Create your views here, class based views
+class IndexView(TemplateView):
+    template_name = "posts/index.html"
